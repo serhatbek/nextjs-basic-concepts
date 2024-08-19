89cab4a72de08270eec8bdf8e2d6732640e8f9dd
serhatbek <serhat.bekk@gmail.com>
2024-08-19 16:20:57 +0300

    links

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <div className="bg-red-400">fdghf</div>
+      <div className="bg-red-400 flex gap-2">
+        <Link href="/doc">Doc</Link>
+        <Link href="/dashboard">Dashboard</Link>
+        <Link href="/dashboard/settings">Settings</Link>
+      </div>
     </>
   );
 }
