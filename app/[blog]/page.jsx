8ec42fcba33a13422a843a738594add5d6806b3c
serhatbek <serhat.bekk@gmail.com>
2024-08-21 09:38:29 +0300

    Static Routes & Dynamic Routes

---
+++ app/[blog]/page.jsx
@@ -0,0 +1,5 @@
+const Blog = ({ params }) => {
+  return <div>Blog Sayfası: {params.blog}</div>;
+};
+
+export default Blog;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Home() {
-  return (
-    <>
-      <main className="bg-red-400 flex gap-2">Home</main>
-    </>
-  );
-}
