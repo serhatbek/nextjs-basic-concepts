e445ee94e38537d654d5304dfe752e29ac40b418
serhatbek <serhat.bekk@gmail.com>
2024-08-23 16:01:54 +0300

    🚧 link component

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,27 @@
+import Link from "next/link";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Link href="/about">About sayfasına git</Link>
+      <br />
+      <Link href="/docs/post-1">Post 1</Link>
+      <br />
+      <Link href="/docs/post-1/post-2">Post 2</Link>
+      <br />
+      <Link
+        href={{
+          pathname: "/about",
+          query: {
+            name: "Next.js",
+            surname: "ff",
+          },
+        }}
+      >
+        Obje ile git
+      </Link>
+    </div>
+  );
 };
 
 export default HomePage;
