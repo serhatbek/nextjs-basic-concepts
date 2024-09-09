e03d7fd3443140071b31e2efa1aae18ad30cd6d5
serhatbek <serhat.bekk@gmail.com>
2024-09-09 23:04:25 +0300

    ✨ CSS Modules

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,15 @@
+import styles from "./homepage.module.css";
+
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Home Page</h1>
+      <p className={styles["home-p"]}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt,
+        dignissimos?
+      </p>
+    </div>
+  );
 };
 
 export default HomePage;
--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,5 +1,21 @@
+import styles from "./test.module.css";
+
 const TestRoute = () => {
-  return <div>TestRoute</div>;
+  const bg = false;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Test Page</h1>
+      <p className={styles["home-p"]}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt,
+        dignissimos?
+      </p>
+
+      <p className={styles[`${bg === true ? "home-p" : "test-p"}`]}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt,
+        dignissimos?
+      </p>
+    </div>
+  );
 };
 
 export default TestRoute;
