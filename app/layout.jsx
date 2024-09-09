05a58f7e43aeaa4e7eb7362a06ec4be41a08c1d1
serhatbek <serhat.bekk@gmail.com>
2024-09-09 23:13:52 +0300

    ✨ Global CSS - reset.css

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
-
+import "../styles/resets.css"; // Always first
+import "../styles/global.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
